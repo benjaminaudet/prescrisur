4d1674ff1b8dbd1e942477050a20e02df3ecb1a7
Pierrick Boutruche <pboutruche@octo.com>
2016-04-06 17:58:10 +0200

    removed ui for data management (will do it later)

--- prescrisur/views.py
+++ prescrisur/views.py
@@ -8,12 +8,3 @@ from prescrisur.update import SpecialityUpdater
 @app.route('/')
 def home():
 	return render_template('index.html')
-
-
-@app.route('/data')
-@app.route('/data/<type>')
-def data(type=None):
-	if type:
-		SpecialityUpdater().execute()
-		flash('Specialites mises a jour !', 'success')
-	return render_template('data.html')
