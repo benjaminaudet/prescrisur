e0250d35e88265e15344629e14c4294b226cfe25
Pierrick Boutruche <pboutruche@octo.com>
2016-04-19 16:41:23 +0200

    recursive tree with sublevel deletion

--- front/app/controllers/pathology.js
+++ front/app/controllers/pathology.js
@@ -4,27 +4,40 @@ angular.module('prescrisurApp.controllers')
 	'$scope',
 
 	function($scope) {
-		$scope.delete = function(data) {
-			data.levels = [];
+		var getLevelName = function(parentLevelName, $index) {
+			return parentLevelName + ($index + 1) + '.';
 		};
 
-		$scope.add = function(data) {
-			var post = data.levels.length + 1;
-			var levelName = data.levelName + post + '.';
+		$scope.delete = function(data, $index) {
+			var levelName = getLevelName(data.parentLevelName, $index);
+			var splitLevel = levelName.split('.');
+			splitLevel = splitLevel.slice(0, -1);
+			var toDel = splitLevel.pop() - 1;
+			var levelToGo = $scope.pathology.levels;
+			splitLevel.forEach(function(i) {
+				levelToGo = levelToGo[i-1].levels;
+			});
+			levelToGo.splice(toDel, 1);
+		};
+
+		$scope.add = function(data, $index) {
+			var levelName = getLevelName(data.parentLevelName, $index);
 			var depth = data.depth + 1;
-			data.levels.push({levelName: levelName, depth: depth,levels: []});
+			data.levels.push({parentLevelName: levelName, depth: depth, levels: []});
 		};
 
-		$scope.addRoot = function(data) {
-			var post = $scope.pathology.levels.length + 1;
-			var levelName = post + '.';
-			$scope.pathology.levels.push({levelName: levelName, depth: 1, levels:[]});
+		$scope.addRoot = function() {
+			$scope.pathology.levels.push({parentLevelName: '', depth: 1, levels:[]});
+		};
+
+		$scope.submit = function() {
+			console.log($scope.pathology);
 		};
 
 		$scope.pathology = {
 			name: null,
 			levels: [
-				{levelName: '1.', depth: 1, levels: []}
+				{parentLevelName: '', depth: 1, levels: []}
 			]
 		};
 	}
