235269a7239b7f6432b4e40cb5a4a555113c67b7
Pierrick Boutruche <pboutruche@octo.com>
2016-04-22 15:40:42 +0200

    removed speciality service

--- front/app/services/models.js
+++ front/app/services/models.js
@@ -8,15 +8,6 @@ angular.module('prescrisurApp.modelServices', ['ngResource'])
 	}
 ])
 
-.factory("SpecialityService", [
-	'$window',
-	'$stateParams',
-
-	function($window, $stateParams) {
-		return $window.location.href = 'http://base-donnees-publique.medicaments.gouv.fr/extrait.php?specid='+$stateParams.id;
-	}
-])
-
 .factory('SubstanceService', ['$resource',
 	function($resource){
 		return $resource('/api/substances/:id');
