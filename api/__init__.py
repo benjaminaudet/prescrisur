0afd0fd9ae0e0172ee9f3cfa9142bb4a8dca81b1
Pierrick Boutruche <pboutruche@octo.com>
2016-05-19 13:38:50 +0200

    enable cors on all routes

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_mail import Mail
+from flask.ext.cors import CORS
 from flask.ext.login import LoginManager
 
 from json_encoder import ModelJSONEncoder
 
 mail = Mail()
+cors = CORS()
 login_manager = LoginManager()
 
 
@@ -14,10 +16,13 @@ def create_app(config_module):
 	app_.json_encoder = ModelJSONEncoder
 	app_.config.from_object(config_module)
 
+	# Import views
 	from views import api
 	app_.register_blueprint(api)
-
+	
+	# Init plugins
 	mail.init_app(app_)
+	cors.init_app(app_)
 	login_manager.init_app(app_)
 	return app_
 
