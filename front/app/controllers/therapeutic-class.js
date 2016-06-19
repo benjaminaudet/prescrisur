3300128252801e3bae2c326a8a09b07295890fbe
Pierrick Boutruche <pboutruche@octo.com>
2016-06-19 20:58:48 +0200

    search class on class page

--- front/app/controllers/therapeutic-class.js
+++ front/app/controllers/therapeutic-class.js
@@ -6,9 +6,10 @@ angular.module('prescrisurApp.controllers')
 		'$stateParams',
 		'Flash',
 		'PageTitleService',
+		'SearchService',
 		'TherapeuticClassService',
 
-		function($scope, $state, $stateParams, Flash, PageTitleService, TherapeuticClassService) {
+		function($scope, $state, $stateParams, Flash, PageTitleService, SearchService, TherapeuticClassService) {
 			$scope.therapeuticClass = null;
 			
 			TherapeuticClassService.get({id: $stateParams.id}, function(data) {
@@ -30,6 +31,31 @@ angular.module('prescrisurApp.controllers')
 					return 1;
 				}
 				return 2;
+			};
+
+			$scope.search = function($select) {
+				var search = '';
+				if($select) {
+					search = $select.search;
+				}
+				$scope.results = searchMessage('Recherche en cours...');
+				SearchService.get({q: search, searchType: 'classes'}, function(data) {
+					$scope.results = data.data;
+					if ($scope.results.length == 0) {
+						$scope.results = searchMessage('Aucun résultat');
+					}
+				});
+			};
+
+			$scope.goTo = function($select) {
+				$state.go('classes', {id: $select.selected._id});
+			};
+
+			var searchMessage = function(msg) {
+				return [{
+					_id: null,
+					name: msg
+				}];
 			};
 		}
 	]);
