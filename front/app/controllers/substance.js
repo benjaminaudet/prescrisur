a2ea365be66f35bc53fc19895440d9b7144000f0
Pierrick Boutruche <pboutruche@octo.com>
2016-06-19 20:48:29 +0200

    search substance on substance page

--- front/app/controllers/substance.js
+++ front/app/controllers/substance.js
@@ -7,10 +7,11 @@ angular.module('prescrisurApp.controllers')
 	'$timeout',
 	'Flash',
 	'PageTitleService',
+	'SearchService',
 	'SubstanceService',
 	'PathologySubstanceService',
 
-	function($scope, $state, $stateParams, $timeout, Flash, PageTitleService, SubstanceService, PathologySubstanceService) {
+	function($scope, $state, $stateParams, $timeout, Flash, PageTitleService, SearchService, SubstanceService, PathologySubstanceService) {
 		$scope.substance = null;
 		$scope.pathologies = [];
 
@@ -26,6 +27,31 @@ angular.module('prescrisurApp.controllers')
 
 		PathologySubstanceService.get({ id: $stateParams.id }, function(data) {
 			$scope.pathologies = data.data;
-		})
+		});
+
+		$scope.search = function($select) {
+			var search = '';
+			if($select) {
+				search = $select.search;
+			}
+			$scope.results = searchMessage('Recherche en cours...');
+			SearchService.get({q: search, searchType: 'substances'}, function(data) {
+				$scope.results = data.data;
+				if ($scope.results.length == 0) {
+					$scope.results = searchMessage('Aucun résultat');
+				}
+			});
+		};
+
+		$scope.goTo = function($select) {
+			$state.go('substances', {id: $select.selected._id});
+		};
+
+		var searchMessage = function(msg) {
+			return [{
+				_id: null,
+				name: msg
+			}];
+		};
 	}
 ]);
