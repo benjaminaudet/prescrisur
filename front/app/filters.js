1a617213f34d2a9362d21e4526a46d87ca5e853a
Pierrick Boutruche <pboutruche@octo.com>
2016-06-19 23:22:11 +0200

    accord experts

--- front/app/filters.js
+++ front/app/filters.js
@@ -3,8 +3,15 @@ angular.module('prescrisurApp.filters', [])
 .filter('gradify', ['$state',
 	function($state) {
 		return function(input) {
-			var regx = /(Grade (?:A|B|C|X[a-z0-9]{2}))/gi;
-			return input.replace(regx, '<a class="grade" href="#/pages/presentation">$1</a>');
+			// Grade A/B/C + Accord d'experts
+			var regxABC = /(Grade (?:A|B|C)|AE|Accords d'experts)/gi;
+			input = input.replace(regxABC, '<a class="grade" href="#/pages/presentation">$1</a>');
+
+			// Grade X..
+			var regxX = /(Grade X[a-z0-9]{2})/gi;
+			input = input.replace(regxX, '<a class="grade">$1</a>');
+
+			return input;
 		};
 	}
 ]);
