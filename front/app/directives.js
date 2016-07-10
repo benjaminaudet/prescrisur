c51362898c50d5739d37d80efd394eeed873136c
Pierrick Boutruche <pboutruche@octo.com>
2016-07-10 14:34:28 +0200

    grade tooltip

--- front/app/directives.js
+++ front/app/directives.js
@@ -10,4 +10,18 @@ angular.module('prescrisurApp.directives', [])
 			title: '@'
 		}
 	};
+})
+
+.directive("ngBindHtmlCompile", function($compile, $sce){
+	return {
+		restrict: "A",
+		link: function(scope, element, attrs){
+			scope.$watch($sce.parseAsHtml(attrs.ngBindHtmlCompile), function(html){
+				var el = angular.element("<div>").html(html);
+				element.empty();
+				element.append(el.children());
+				$compile(element.contents())(scope);
+			})
+		}
+	};
 });
--- front/app/filters.js
+++ front/app/filters.js
@@ -1,17 +1,30 @@
 angular.module('prescrisurApp.filters', [])
 
-.filter('gradify', ['$state',
-	function($state) {
+.filter('gradify', ['$state', '$sce',
+	function($state, $sce) {
 		return function(input) {
 			// Grade A/B/C + Accord d'experts
 			var regxABC = /(Grade (?:A|B|C)|AE|Accords d'experts)/gi;
-			input = input.replace(regxABC, '<a class="grade" href="#/pages/presentation">$1</a>');
+			var matches = regxABC.exec(input);
+
+			var labels = {
+				"Grade A": "preuve scientifique établie",
+				"Grade B": "présomption scientifique",
+				"Grade C": "faible niveau de preuve scientifique",
+				"AE": "approbation, en l’absence de données scientifiques disponibles, d’au moins 80 % des membres du groupe de travail",
+				"Accords d'experts": "approbation, en l’absence de données scientifiques disponibles, d’au moins 80 % des membres du groupe de travail"
+			};
+			var link = "pages.read({id: 'presentation'})";
+
+			if (matches) {
+				input = input.replace(regxABC, '<a uib-tooltip="'+labels[matches[1]]+'" class="grade" ui-sref="'+link+'">$1</a>');
+			}
 
 			// Grade X..
 			var regxX = /(Grade X[a-z0-9]{2})/gi;
 			input = input.replace(regxX, '<a class="grade">$1</a>');
 
-			return input;
+			return $sce.trustAsHtml(input);
 		};
 	}
 ]);
