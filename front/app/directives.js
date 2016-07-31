6f9978bf9a48495c03fae7dc14f77e3203718056
Pierrick Boutruche <pboutruche@octo.com>
2016-07-31 16:54:33 +0200

    spec status has tooltip and is sup-ed when mobile view

--- front/app/directives.js
+++ front/app/directives.js
@@ -12,6 +12,25 @@ angular.module('prescrisurApp.directives', [])
 	};
 })
 
+.directive('specStatus', function () {
+	return {
+		restrict: 'E',
+		templateUrl: 'front/app/templates/partials/spec-status.html',
+		scope: {
+			status: '@'
+		},
+		link: function (scope, elem, attr) {
+			var status = attr.status.toLowerCase();
+			var statusLabels = {
+				g: 'Générique',
+				r: 'Princeps'
+			};
+
+			scope.label = statusLabels[status];
+		}
+	};
+})
+
 .directive("ngBindHtmlCompile", function($compile, $sce){
 	return {
 		restrict: "A",
